5b2dc3f95bc36de6a7d59d6ae4ed08978b706f73
jbozikovic <jbozikovic@gmail.com>
2023-03-31 11:10:04 +0200

    Initial Commit

---
+++ TelTechApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelTechApp
+//
+//  Created by Jurica Bozikovic on 31.03.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
